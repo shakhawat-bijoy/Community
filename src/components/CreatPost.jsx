38a6557a0cb07125cb065c3d9be76a65fecc5798
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-06-28 21:43:41 +0600

    Responsive

--- src/components/CreatPost.jsx
+++ src/components/CreatPost.jsx
@@ -4,12 +4,12 @@ import Memoji1 from '/Memoji1.png'
 
 const CreatPost = () => {
   return (
-    <div className='lg:w-[785px] w-[335px] flex items-center gap-5 bg-[#262d34] lg:p-5 p-[14px] rounded-lg mx-auto'>
+    <div className='lg:w-[785px] w-[335px] flex items-center gap-5 bg-[#262d34] lg:p-5 p-2 rounded-lg mx-auto'>
         <div>
             <Image src={Memoji1}/>
         </div>
 
-        <div class="lg:w-[745px] w-[174px] rounded bg-[#2c353d]">
+        <div class="lg:w-[745px] w-[200px] rounded bg-[#2c353d]">
             <input rows={1}  class="w-full border-none bg-transparent p-3 text-white outline-none resize-none" placeholder="Let’s share what going on your mind..." />
         </div>
             <button className="rounded py-3 lg:w-[112px] w-[83px] lg:text-sm text-xs font-medium leading-5 bg-[#FF6934] text-[#fefeff]">
