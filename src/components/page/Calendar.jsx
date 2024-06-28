c267cc3ed8ea3dbde8051ea1f96047c371b25796
Shakhawat Bijoy <piyash2003b@gmail.com>
2024-06-28 21:13:03 +0600

    Calender added

--- src/components/page/Calendar.jsx
+++ src/components/page/Calendar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactCalendar from 'react-calendar'; // Renamed import to avoid naming conflict
+import 'react-calendar/dist/Calendar.css';
 
-const Calendar  = () => {
+const MyCalendar = () => {
+  const [value, onChange] = useState(new Date());
+
   return (
-    <div className='bg-[#1e242a] h-[100vh] text-white flex items-center justify-center p-20'>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, qui ratione ipsam, excepturi asperiores voluptatibus repellendus in, possimus cupiditate veritatis cumque quas ad quaerat sint aliquam reprehenderit iusto quibusdam obcaecati. </div>
-  )
+    <div className='bg-[#1e242a] h-screen text-white flex items-center justify-center p-20'>
+      <ReactCalendar className="text-black" onChange={onChange} value={value} />
+    </div>
+  );
 }
 
-export default Calendar 
+export default MyCalendar;
